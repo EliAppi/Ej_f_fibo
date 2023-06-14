48d486703afec0a54c52b9d3800aed38e0a90a11
EliAppi <ing.eliana.pa@gmail.com>
2023-06-14 09:38:46 -0500

    agrego funcion
    changed author

--- Ej_f_fibo.js
+++ Ej_f_fibo.js
@@ -1,33 +1,35 @@
-let n=Number("21");
+let n;
 let suma=0;
 let j=0;
 let k=0;
 let secuFibo=[0,1];
 
-for (let i=0;i<n+2;i++){
-    suma=secuFibo[i]+secuFibo[i+1];
-    secuFibo.push(suma);
+function creaFibo(n){
+    for (let i=0;i<n+2;i++){
+        suma=secuFibo[i]+secuFibo[i+1];
+        secuFibo.push(suma);
+    }
+    for (x of secuFibo){
+        if (n==1){
+            console.log("Inmediatamente mayor: "+0);
+            console.log("Inmediatamente menor: "+2);
+            break;
+        }
+        else if (x==n){
+            console.log("Inmediatamente mayor: "+secuFibo[k+1]);
+            console.log("Inmediatamente menor: "+secuFibo[k-1]);
+            break;
+        }
+        else if (x>n){
+            console.log("Inmediatamente mayor: "+x);
+            console.log("Inmediatamente menor: "+secuFibo[k-1]);
+            break;
+        }    
+        else{
+            k++;
+        }
+    }
 }
+creaFibo(13);
 //console.log(secuFibo);
 
-//let pos=secuFibo.indexOf(n);
-for (x of secuFibo){
-    if (n==1){
-        console.log("Inmediatamente mayor: "+0);
-        console.log("Inmediatamente menor: "+2);
-        break;
-    }
-    else if (x==n){
-        console.log("Inmediatamente mayor: "+secuFibo[k+1]);
-        console.log("Inmediatamente menor: "+secuFibo[k-1]);
-        break;
-    }
-    else if (x>n){
-        console.log("Inmediatamente mayor: "+x);
-        console.log("Inmediatamente menor: "+secuFibo[k-1]);
-        break;
-    }    
-    else{
-        k++;
-    }
-}
